af36f585e30f9101df23bd0d8e0ddc0b090aceb2
Jonas Charfreitag <jcharfreitag@cs.uni-bonn.de>
2020-11-09 11:47:06 +0100

    Make knapsack dev compliant.

--- geco/mips/knapsack.py
+++ geco/mips/knapsack.py
@@ -4,31 +4,33 @@ import math
 import pyscipopt as scip
 
 
-def knapsack(n, seed=0):
+def yang(n, seed):
     """
-    This function is based on the MIP generation techniques described in
-    Yu Yang, Natashia Boland, Bistra Dilkina, Martin Savelsbergh,
-    "Learning Generalized Strong Branching for Set Covering,
-    Set Packing, and 0-1 Knapsack Problems", 2020.
+        This function is based on the MIP generation techniques described in
+        Yu Yang, Natashia Boland, Bistra Dilkina, Martin Savelsbergh,
+        "Learning Generalized Strong Branching for Set Covering,
+        Set Packing, and 0-1 Knapsack Problems", 2020.
     """
     random.seed(seed)
-    model = scip.Model("Yang Knapsack")
 
     def draw_value():
         return random.randint(1, 10 * n)
 
-    # add variables and their cost
-    vars = []
-    for i in range(n):
-        profit = draw_value()
-        var = model.addVar(lb=0, ub=1, obj=profit, vtype="B")
-        vars.append(var)
-
-    # add constraints
-    weights = [draw_value() for _ in vars]
-    weighted_vars = (weight * var for (weight, var) in zip(weights, vars))
+    profit = [draw_value() for _ in range(n)]
+    weights = [draw_value() for _ in range(n)]
     capacity = math.floor(sum(weights) / 5)
-    model.addCons(scip.quicksum(weighted_vars) <= capacity)
+
+    return profit, weights, capacity
+
+
+def knapsack(weights, profits, capacity, name="Knapsack"):
+    assert len(weights) == len(profits)
+
+    model = scip.Model(name)
+    # add variables and their cost
+    variables = [model.addVar(lb=0, ub=1, obj=profit, vtype="B") for profit in profits]
+    # add constraints
+    model.addCons(scip.quicksum(weight * variable for weight, variable in zip(weights, variables)) <= capacity)
 
     model.setMaximize()
 
