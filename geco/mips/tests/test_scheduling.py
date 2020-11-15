afe788905a062a751ff18288237302c306073271
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2020-11-15 17:55:20 +0100

    Use better name for scip model in tests

--- geco/mips/tests/test_scheduling.py
+++ geco/mips/tests/test_scheduling.py
@@ -5,11 +5,11 @@ def test_late_tasks_formulation():
     main_params = number_of_facilities, number_of_tasks, time_steps = 3, 10, 100
     params = p, C, c, R, d = generate_params(*main_params[:-1], seed=0)[:-1]
 
-    m_1 = hooker_late_tasks_formulation(
+    model = hooker_late_tasks_formulation(
         *main_params, *params
     )
     assert (
-            m_1.getNVars()
+            model.getNVars()
             == number_of_facilities * number_of_tasks * time_steps + number_of_tasks
     )
     constraints_lowerbound = (
@@ -23,21 +23,21 @@ def test_late_tasks_formulation():
             + number_of_facilities * time_steps
             + number_of_facilities * number_of_tasks * time_steps
     )
-    assert constraints_lowerbound <= m_1.getNConss() <= constraints_upperbound
-    assert m_1.getObjectiveSense() == "minimize"
+    assert constraints_lowerbound <= model.getNConss() <= constraints_upperbound
+    assert model.getObjectiveSense() == "minimize"
 
 
 def test_heinz_formulation():
     main_params = number_of_facilities, number_of_tasks = 3, 20
     params = p, C, c, R, d, r = generate_params(*main_params, seed=0)
     time_steps = int(max(d.values()) - min(R))
-    m_1 = heinz_formulation(*main_params, *params)
+    model = heinz_formulation(*main_params, *params)
     x_vars_count = number_of_facilities * number_of_tasks
     y_vars_lowerbound = number_of_facilities * number_of_tasks
     y_vars_upperbound = number_of_facilities * number_of_tasks * time_steps
     assert (
             x_vars_count + y_vars_lowerbound
-            <= m_1.getNVars()
+            <= model.getNVars()
             <= x_vars_count + y_vars_upperbound
     )
     constraints_lowerbound = (
@@ -56,7 +56,7 @@ def test_heinz_formulation():
             + number_of_facilities * number_of_tasks
             + number_of_facilities * number_of_tasks * time_steps
     )
-    assert constraints_lowerbound <= m_1.getNConss() <= constraints_upperbound
-    assert m_1.getObjectiveSense() == "minimize"
+    assert constraints_lowerbound <= model.getNConss() <= constraints_upperbound
+    assert model.getObjectiveSense() == "minimize"
 
-    m_1.optimize()
+    model.optimize()
