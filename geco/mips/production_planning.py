11d27e7f4636c0f27a2ffcc8668cd5901c3b3bfe
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2020-11-16 09:39:35 +0100

    Use py_random_state & separate param generation

--- geco/mips/production_planning.py
+++ geco/mips/production_planning.py
@@ -1,8 +1,11 @@
-import pyscipopt as scip
 import random
 
+import pyscipopt as scip
+from networkx.utils import py_random_state
 
-def tang(T, M=100, initial_storage=0, final_storage=20, seed=0):
+
+@py_random_state(1)
+def tang_instance(T, seed=0):
     """Generates a production planning instance as described in A.2 in
     Tang, Y., Agrawal, S., & Faenza, Y. (2019). Reinforcement learning for integer
     programming: Learning to cut. arXiv preprint arXiv:1906.04859.
@@ -21,14 +24,15 @@ def tang(T, M=100, initial_storage=0, final_storage=20, seed=0):
     production_vars = []
     produce_or_not_vars = []
     storage_vars = []
+    M, initial_storage, final_storage, p, h, q, d = tang_params(T, seed)
     for i in range(T + 1):
-        var = model.addVar(lb=0, ub=None, obj=random.randint(1, 10), name=f"x_{i}", vtype="I")
+        var = model.addVar(lb=0, ub=None, obj=p[i], name=f"x_{i}", vtype="I")
         production_vars.append(var)
 
-        var = model.addVar(lb=0, ub=1, obj=random.randint(1, 10), name=f"y_{i}", vtype="B")
+        var = model.addVar(lb=0, ub=1, obj=h[i], name=f"y_{i}", vtype="B")
         produce_or_not_vars.append(var)
 
-        var = model.addVar(lb=0, ub=None, obj=random.randint(1, 10), name=f"s_{i}", vtype="I")
+        var = model.addVar(lb=0, ub=None, obj=q[i], name=f"s_{i}", vtype="I")
         storage_vars.append(var)
 
     # remove unneeded var
@@ -36,8 +40,7 @@ def tang(T, M=100, initial_storage=0, final_storage=20, seed=0):
 
     # add constraints
     for i in range(1, T + 1):
-        d_i = random.randint(1, 10)
-        model.addCons(storage_vars[i - 1] + production_vars[i] == d_i + storage_vars[i])
+        model.addCons(storage_vars[i - 1] + production_vars[i] == d[i] + storage_vars[i])
         model.addCons(production_vars[i] <= M * produce_or_not_vars[i])
 
     model.addCons(storage_vars[0] == initial_storage)
@@ -48,3 +51,18 @@ def tang(T, M=100, initial_storage=0, final_storage=20, seed=0):
     return model
 
 
+@py_random_state(1)
+def tang_params(T, seed=0):
+    initial_storage = 0
+    final_storage = 20
+    M = 100
+    p = []
+    h = []
+    q = []
+    d = []
+    for i in range(T + 1):
+        p.append(seed.randint(1, 10))
+        h.append(seed.randint(1, 10))
+        q.append(seed.randint(1, 10))
+        d.append(seed.randint(1, 10))
+    return M, initial_storage, final_storage, p, h, q, d
