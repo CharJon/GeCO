9117ae81550846c850085e9d46e9dba355ccb620
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2020-11-16 09:39:54 +0100

    Test param generation seeding

--- geco/mips/tests/test_production_planning.py
+++ geco/mips/tests/test_production_planning.py
@@ -3,7 +3,14 @@ from geco.mips.production_planning import *
 
 def test_tang():
     T = 100
-    m_1 = tang(T)
-    assert m_1.getNVars() == (T + 1) * 3 - 1
-    assert m_1.getNConss() == 2 * T + 2
-    assert m_1.getObjectiveSense() == 'minimize'
+    model = tang_instance(T)
+    assert model.getNVars() == (T + 1) * 3 - 1
+    assert model.getNConss() == 2 * T + 2
+    assert model.getObjectiveSense() == 'minimize'
+
+
+def test_seeding():
+    T = 100
+    params1 = tang_params(T, seed=1)
+    params2 = tang_params(T, seed=2)
+    assert params1 != params2
