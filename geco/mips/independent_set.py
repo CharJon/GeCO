38184ec39ba5cd605d4a3dc4c8e39a414d51e669
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2020-11-25 17:59:51 +0100

    Gasse instance generator

--- geco/mips/independent_set.py
+++ geco/mips/independent_set.py
@@ -1,12 +1,10 @@
-import itertools
-
 import networkx as nx
 import pyscipopt as scip
 
 
 def independent_set(graph: nx.Graph, name: str = "Independent Set") -> scip.Model:
     """
-    Generates an independent set according to [1].
+    Generates an independent set instance according to [1].
 
     Parameters
     ----------
@@ -37,3 +35,105 @@ def independent_set(graph: nx.Graph, name: str = "Independent Set") -> scip.Mode
     model.setMaximize()
 
     return model
+
+
+def _get_cliques(graph: nx.Graph) -> list:
+    """
+    Partition the graph into cliques using a greedy algorithm, this code is
+    based on the code from [1].
+
+    Returns
+    -------
+    cliques: list of sets
+        The resulting clique partition.
+
+    References
+    ----------
+    .. [1] https://github.com/ds4dm/learn2branch/blob/master/01_generate_instances.py
+    """
+    cliques = []
+
+    # sort nodes in descending order of degree
+    leftover_nodes = sorted(list(graph.nodes), key=lambda node: - graph.degree[node])
+
+    while leftover_nodes:
+        clique_center, leftover_nodes = leftover_nodes[0], leftover_nodes[1:]
+        clique = {clique_center}
+        neighbors = set(graph.neighbors(clique_center)).intersection(leftover_nodes)
+        densest_neighbors = sorted(neighbors, key=lambda node: - graph.degree[node])
+        for neighbor in densest_neighbors:
+            if all([neighbor in graph.neighbors(clique_node) for clique_node in clique]):
+                clique.add(neighbor)
+        cliques.append(clique)
+        leftover_nodes = [node for node in leftover_nodes if node not in clique]
+
+    return cliques
+
+
+def clique_independent_set(graph: nx.Graph, name: str = "Clique Independent Set"):
+    """
+    Generates an independent set instance according to [1, 4.6.4].
+
+    Parameters
+    ----------
+    graph: nx.Graph
+        Networkx undirected graph
+    name: str
+        name of the generated model
+
+    Returns
+    -------
+    model: scip.Model
+        pyscipopt model of the generated instance
+
+    References
+    ----------
+    .. [1] David Bergman, Andre A. Cire, Willem-Jan Van Hoeve, and John Hooker. Decision diagrams
+    for optimization. Springer, 2016.
+    """
+    model = scip.Model(name)
+
+    cliques = _get_cliques(graph)
+
+    vars = {
+        str(node): model.addVar(lb=0, ub=1, obj=1, name=str(node), vtype="B")
+        for node in graph.nodes
+    }
+
+    for clique in cliques:
+        model.addCons(scip.quicksum(vars[str(node)] for node in clique) <= 1)
+
+    model.setMaximize()
+
+    return model
+
+
+def gasse_params(n: int, p: float, seed=0) -> nx.Graph:
+    return nx.generators.erdos_renyi_graph(n, p, seed)
+
+
+def gasse_instance(n: int, p: float, seed=0) -> scip.Model:
+    """
+    Generates a maximum independent set instance as described in [1].
+
+    Parameters
+    ----------
+    n: int
+        number of nodes.
+    p: float
+        edge probability
+    seed: int, random state or None
+        randomization seed
+
+    Returns
+    -------
+    model: scip.Model
+        pyscipopt model of the instance.
+
+    References
+    ----------
+    .. [1] "Exact Combinatorial Optimization with Graph Convolutional Neural Networks" (2019)
+      Maxime Gasse, Didier Chételat, Nicola Ferroni, Laurent Charlin and Andrea Lodi
+      Advances in Neural Information Processing Systems 32 (2019)
+    """
+    return clique_independent_set(gasse_params(n, p, seed))
--- geco/mips/tests/test_independent_set.py
+++ geco/mips/tests/test_independent_set.py
@@ -1,3 +1,5 @@
+import itertools
+
 import pytest
 
 from geco.mips.independent_set import *
@@ -26,3 +28,49 @@ def test_simple_instance():
     model.optimize()
     assert model.getStatus() == "optimal"
     assert model.getObjVal() == 1
+
+
+@pytest.mark.parametrize(
+    "graph",
+    [
+        nx.generators.complete_graph(3),
+        nx.generators.complete_graph(10),
+        nx.generators.complete_graph(50),
+    ],
+)
+def test_clique_independent_set(graph):
+    model = clique_independent_set(graph)
+    assert model.getNVars() == len(graph.nodes)
+    assert model.getNConss() <= len(graph.edges)
+    assert model.getObjectiveSense() == "maximize"
+
+
+def test_clique_simple_instance():
+    graph = nx.generators.complete_graph(3)
+    model = clique_independent_set(graph)
+    model.hideOutput()
+    model.optimize()
+    assert model.getStatus() == "optimal"
+    assert model.getObjVal() == 1
+
+
+@pytest.mark.parametrize(
+    "n,p,seed", itertools.product([10, 50], [0.5, 0.9], [0, 1, 1337, 53115])
+)
+def test_gasse(n, p, seed):
+    model = gasse_instance(n, p, seed)
+    assert model.getNVars() == n
+    assert model.getNConss() <= n * (n - 1) / 2
+    assert model.getObjectiveSense() == "maximize"
+
+
+@pytest.mark.parametrize(
+    "n,p,seed1,seed2", itertools.product([10, 50], [0.5, 0.9], [0, 1, 1337, 53115], [0, 1, 1337, 53115])
+)
+def test_gasse_seeding(n, p, seed1, seed2):
+    graph1 = gasse_params(n, p, seed=seed1)
+    graph2 = gasse_params(n, p, seed=seed2)
+    same_edges = set(graph1.edges) == set(graph2.edges)
+    same_seeds_produce_same_params = seed1 == seed2 and same_edges
+    different_seeds_produce_different_params = seed1 != seed2 and not same_edges
+    assert same_seeds_produce_same_params or different_seeds_produce_different_params
