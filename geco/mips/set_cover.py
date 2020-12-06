ef17a796f9f8e3b5d1171a7228882d1ccf04816f
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2020-12-06 18:43:27 +0100

    Set cover expansion

--- geco/mips/set_cover.py
+++ geco/mips/set_cover.py
@@ -1,9 +1,8 @@
 import pyscipopt as scip
-
 from networkx.utils import py_random_state
 
 
-@py_random_state(1)
+@py_random_state(-1)
 def yang_instance(m, seed=0):
     """Yu Yang, Natashia Boland, Bistra Dilkina, Martin Savelsbergh,
     "Learning Generalized Strong Branching for Set Covering,
@@ -12,7 +11,7 @@ def yang_instance(m, seed=0):
     return set_cover(*yang_parameter(m, seed))
 
 
-@py_random_state(1)
+@py_random_state(-1)
 def yang_parameter(m, seed=0):
     """Yu Yang, Natashia Boland, Bistra Dilkina, Martin Savelsbergh,
     "Learning Generalized Strong Branching for Set Covering,
@@ -44,5 +43,43 @@ def set_cover(costs, sets):
         model.addCons(scip.quicksum(variables[i] for i in s) >= 1)
 
     model.setMinimize()
+
+    return model
+
+
+def expand_set_cover(model, new_costs, new_sets):
+    """
+    Implements the expansion from an existing set cover instance as described in [1]
+
+    Parameters
+    ----------
+    model: scip.Model
+        scip model of the "backbone" set cover instance
+    new_costs: list
+        list of costs for the extra elements
+    new_sets: list
+        list of elements in each new set
+
+    Returns
+    -------
+     model: scip.Model
+         expanded model
+
+    References
+    __________
+    .. [1] Haoran Sun, Wenbo Chen, Hui Li, & Le Song (2021).
+     Improving Learning to Branch via Reinforcement Learning. In Submitted to
+     International Conference on Learning
+    """
+    model = scip.Model(sourceModel=model)
+
+    previous_vars = model.getVars()
+    variables = previous_vars + [
+        model.addVar(lb=0, ub=1, obj=c, name=f"v_{i + len(previous_vars)}", vtype="B")
+        for i, c in enumerate(new_costs)
+    ]
+
+    for s in new_sets:
+        model.addCons(scip.quicksum(variables[i] for i in s) >= 1)
 
     return model
--- geco/mips/tests/test_set_cover.py
+++ geco/mips/tests/test_set_cover.py
@@ -40,3 +40,21 @@ def test_yang_parameter(m, seed1, seed2):
     same_seeds_produce_same_params = seed1 == seed2 and params1 == params2
     different_seeds_produce_different_params = seed1 != seed2 and params1 != params2
     assert same_seeds_produce_same_params or different_seeds_produce_different_params
+
+
+def test_expand_set_cover():
+    base_model = set_cover([1, 1, 1], [{0}, {1}, {2}])
+    new_costs = [1, 1]
+    new_sets = [{3}, {4}]
+    expanded_model = expand_set_cover(base_model, new_costs, new_sets)
+    base_model.hideOutput()
+    expanded_model.hideOutput()
+    base_model.optimize()
+    expanded_model.optimize()
+    # test that base model didn't change
+    assert base_model.getStatus() == "optimal"
+    assert base_model.getObjVal() == 3
+
+    # test expanded model
+    assert expanded_model.getStatus() == "optimal"
+    assert expanded_model.getObjVal() == 5
