e2ad6bafcf62f4b967a97396431bbbe550a002a5
Jonas Charfreitag <jcharfreitag@cs.uni-bonn.de>
2020-12-10 10:55:54 +0100

    Also make sure models from generator are different.

--- geco/tests/test_generator.py
+++ geco/tests/test_generator.py
@@ -20,9 +20,15 @@ def test_generate():
 
 
 def test_generate_n():
-    gen = generate_n(lambda seed: cornuejols_instance(6, 3, 2, seed), n=400)
+    n = 50
+    gen = generate_n(lambda seed: cornuejols_instance(6, 3, 2, seed), n=n)
     all_models = []
     for i, model in gen:
         assert type(model) == scip.Model
         all_models.append(model)
-    assert len(all_models) == 400
+    assert len(all_models) == n
+
+    for model1, model2 in itertools.combinations(all_models, 2):
+        obj_coeffs_1 = [v.getObj() for v in model1.getVars()]
+        obj_coeffs_2 = [v.getObj() for v in model2.getVars()]
+        assert obj_coeffs_1 != obj_coeffs_2
