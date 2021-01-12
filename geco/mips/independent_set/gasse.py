19ed2c329a40e6e12e5a05ab2791ff59bc7d8e46
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2021-01-12 11:52:32 +0100

    Independent Set docs improvements

--- geco/mips/independent_set/gasse.py
+++ geco/mips/independent_set/gasse.py
@@ -5,6 +5,29 @@ from geco.mips.independent_set.generic import clique_independent_set
 
 
 def gasse_params(n, p, seed=0):
+    """
+    Generates a maximum independent set instance as described in [1].
+
+    Parameters
+    ----------
+    n: int
+        Number of nodes.
+    p: float
+        Edge probability
+    seed: integer, random_state, or None
+        Indicator of random number generation state
+
+    Returns
+    -------
+    graph: nx.Graph
+        An Erdos-Renyi Networkx graph
+
+    References
+    ----------
+    .. [1] "Exact Combinatorial Optimization with Graph Convolutional Neural Networks" (2019)
+      Maxime Gasse, Didier Chételat, Nicola Ferroni, Laurent Charlin and Andrea Lodi
+      Advances in Neural Information Processing Systems 32 (2019)
+    """
     return nx.generators.erdos_renyi_graph(n, p, seed)
 
 
@@ -15,16 +38,16 @@ def gasse_instance(n, p, seed=0):
     Parameters
     ----------
     n: int
-        number of nodes.
+        Number of nodes.
     p: float
-        edge probability
-    seed: int, random state or None
-        randomization seed
+        Edge probability
+    seed: integer, random_state, or None
+        Indicator of random number generation state
 
     Returns
     -------
     model: scip.Model
-        pyscipopt model of the instance.
+        A pyscipopt model of the instance.
 
     References
     ----------
