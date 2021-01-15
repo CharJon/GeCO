b5b00f9a0044042cafd4d106ef6ce79146faa1b3
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2021-01-15 15:25:47 +0100

    Fix set cover import

--- geco/mips/tests/test_set_cover.py
+++ geco/mips/tests/test_set_cover.py
@@ -3,7 +3,9 @@ import itertools
 
 import pytest
 
-from geco.mips.set_cover import *
+from geco.mips.set_cover.generic import *
+from geco.mips.set_cover.yang import *
+from geco.mips.set_cover.sun import *
 
 """
 Generic Tests
