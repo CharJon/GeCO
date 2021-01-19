491e78ebc0cf90c0059446ad98a2b951cb722d3f
Jonas Charfreitag <jcharfreitag@cs.uni-bonn.de>
2021-01-19 10:24:50 +0100

    Add method to test writing.

--- geco/mips/tests/test_utilities.py
+++ geco/mips/tests/test_utilities.py
@@ -1,5 +1,7 @@
-from geco.mips.utilities.generic import *
+import tempfile
+
 from geco.mips.set_cover.yang import yang_instance
+from geco.mips.utilities.generic import *
 
 
 def test_shuffle():
@@ -10,3 +12,21 @@ def test_shuffle():
     model.optimize()
     shuffled.optimize()
     assert model.getNNodes() != shuffled.getNNodes()
+
+
+def test_saving_shuffled_instance():
+    model = yang_instance(300)
+    shuffled_model = shuffle(model, seed=1)
+
+    original_file = tempfile.NamedTemporaryFile(suffix=".mps")
+    shuffled_file = tempfile.NamedTemporaryFile(suffix=".mps")
+
+    model.writeProblem(original_file.name)
+    shuffled_model.writeProblem(shuffled_file.name)
+
+    with open(original_file.name, 'r') as of, open(shuffled_file.name, 'r') as sf:
+        for line1, line2 in zip(of, sf):
+            if line1 != line2:
+                assert True
+                return
+    assert False
