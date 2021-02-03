89eb46f733c87aa1798d4f1f8cc0e503766ea2eb
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2021-02-03 10:41:41 +0100

    Black

--- geco/mips/tests/test_orlib.py
+++ geco/mips/tests/test_orlib.py
@@ -5,22 +5,23 @@ from geco.mips.loading.orlib import *
 
 def test_read_multiline_correct():
     content = b"""1 2 3.5\n 3 2 1\n"""
-    with open('temp', 'wb') as f:
+    with open("temp", "wb") as f:
         f.write(content)
 
-    with open('temp', 'rb') as f:
+    with open("temp", "rb") as f:
         numbers = read_multiline_numbers(file=f, number_to_read=6)
         assert len(numbers) == 6
         assert list(numbers) == [1, 2, 3.5, 3, 2, 1]
 
-    with open('temp', 'rb') as f, pytest.raises(ValueError):
+    with open("temp", "rb") as f, pytest.raises(ValueError):
         read_multiline_numbers(f, 5)
 
-    os.unlink('temp')
+    os.unlink("temp")
+
 
 def test_read_number():
-    n_1 = read_number(b'1')
-    n_half = read_number(b'.5')
+    n_1 = read_number(b"1")
+    n_half = read_number(b".5")
     assert isinstance(n_1, int) and n_1 == 1
     assert isinstance(n_half, float) and n_half == 0.5
 
