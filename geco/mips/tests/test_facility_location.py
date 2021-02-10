d83fbd0d6f4932393c3922464ad5acbd9e56d795
Jonas Charfreitag <jcharfreitag@cs.uni-bonn.de>
2021-02-10 16:17:27 +0100

    black

--- geco/mips/tests/test_facility_location.py
+++ geco/mips/tests/test_facility_location.py
@@ -11,7 +11,7 @@ def test_capacitated_facility_location():
     model = capacitated_facility_location(n_customers, n_facilities, *instance_params)
     assert model.getNVars() == n_customers * n_facilities + n_facilities
     assert (
-            model.getNConss() == n_customers + n_facilities + 1 + n_customers * n_facilities
+        model.getNConss() == n_customers + n_facilities + 1 + n_customers * n_facilities
     )
     assert model.getObjectiveSense() == "minimize"
     model.hideOutput()
@@ -76,8 +76,7 @@ def test_orlib_solution(instance_name):
     import pandas as pd
 
     df = pd.read_csv(
-        "data/lists/orlib_capacitated_warehouse_location_solutions.csv",
-        comment="#"
+        "data/lists/orlib_capacitated_warehouse_location_solutions.csv", comment="#"
     )
 
     instance = orlib_instance(instance_name + ".txt")
