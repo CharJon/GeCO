bba1a8a4c8e0823a8a490669aad1f51b2f78f192
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2021-02-11 11:58:20 +0100

    More chimera tests

--- geco/graphs/tests/test_chimera.py
+++ geco/graphs/tests/test_chimera.py
@@ -1,32 +1,35 @@
+import itertools
+
 import pytest
 
 from geco.graphs.chimera import *
 
 
 @pytest.mark.parametrize(
-    "m, n, t",
-    [
-        (4, 4, 4),
-        (4, 6, 4),
-    ],
+    "m, n, t", itertools.product(
+        [2, 4, 6],
+        [2, 4, 6],
+        [2, 4, 6],
+    )
 )
 def test_chimera(m, n, t):
     g = chimera_graph(n, m, t)
     assert g.number_of_nodes() == m * n * t * 2
-    assert g.number_of_edges() == 24 * m * n - (m + n) * t
+    assert g.number_of_edges() == n * m * t * t + n * (m - 1) * t + m * (n - 1) * t
 
 
 @pytest.mark.parametrize(
-    "m, n, t",
-    [
-        (4, 4, 4),
-        (4, 6, 4),
-    ],
+    "m, n, t", itertools.product(
+        [4, 6],
+        [4, 6],
+        [4],
+    )
+
 )
 def test_dwave_chimera(m, n, t):
     g = dwave_chimera_graph(m, n, t)
     assert g.number_of_nodes() == m * n * t * 2
-    assert g.number_of_edges() == 24 * m * n - (m + n) * t
+    assert g.number_of_edges() == n * m * t * t + n * (m - 1) * t + m * (n - 1) * t
 
 
 def test_odd_couplers_not_implemented():
