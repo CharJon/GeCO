fc089c1ac91c5a6eef2d2d5a3d6bd34379e72fc9
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2021-03-25 11:08:24 +0100

    Make gasse module accessible

--- geco/mips/set_cover/__init__.py
+++ geco/mips/set_cover/__init__.py
@@ -2,3 +2,4 @@ from geco.mips.set_cover.generic import *
 from geco.mips.set_cover.yang import *
 from geco.mips.set_cover.sun import *
 from geco.mips.set_cover.orlib import *
+from geco.mips.set_cover.gasse import *
